a3b300b628a7c451795a41cbd651274c70ff243e
Ben Scholler <ben.scholler@gmail.com>
2013-02-07 11:11:26 -0800

    Adding a custom command for creating/updating existing metadata given an asset.

---
+++ src/SheKnows/OoyalaApi/Command/UpdateAssetMetadata.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SheKnows\OoyalaApi\Command;
+
+use Guzzle\Service\Command\AbstractCommand;
+
+/**
+ * Updates an asset with specified metadata.
+ *
+ * @guzzle assetId type="string" doc="Asset to update" required="true"
+ * @guzzle data type="array" doc="Metadata to add/replace" required="true"
+ */
+class UpdateAssetMetadata extends AbstractCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function build()
+    {
+        $assetId  = $this->get('assetId');
+        $metadata = $this->get('metadata');
+
+        $url = sprintf("assets/%s/metadata", $assetId);
+
+        $headers = array('Content-type' => 'application/json');
+
+        $data = json_encode($metadata);
+
+        $this->request = $this->client->patch($url, $headers, $data);
+    }
+}
