e1c33741f7aaf70c3fa143b54e95ec2fd2ae0fbd
Ben Scholler <ben.scholler@gmail.com>
2013-02-19 12:42:11 -0800

    Updating command to retrieve label's assets.
    Now returns a hashed array containing the parent labels items, with subkeys to child label IDs that contain items. By setting includeChildren from a top level path, all children will be retrieved with items and next_page links. Should probably update this to use an iterator in the future. At least this gives baseline functionality.

--- src/SheKnows/OoyalaApi/Command/GetLabelsAssets.php
+++ src/SheKnows/OoyalaApi/Command/GetLabelsAssets.php
@@ -21,13 +21,23 @@ class GetLabelsAssets extends AbstractCommand
         $labelId = $this->get('labelId');
         $include = $this->get('include');
 
+        // Need to have label information available in assets if requesting to includeChildren
+        if ($this->get('includeChildren') && !strstr($include, 'labels')) {
+            if (null === $include) {
+                $this->set('include', 'labels');
+            } else {
+                $this->set('include', $include . ',labels');
+            }
+        }
+
         $url = sprintf("labels/%s/assets", $labelId);
+        if (null !== $this->get('include')) {
+            $url .= '?include=' . $this->get('include');
+        }
 
         $headers = array('Content-type' => 'application/json');
 
-        $data = json_encode($metadata);
-
-        $this->request = $this->client->patch($url, $headers, $data);
+        $this->request = $this->client->get($url, $headers);
     }
 
     /**
@@ -35,23 +45,38 @@ class GetLabelsAssets extends AbstractCommand
      */
     protected function process()
     {
-        return parent::process();
+        parent::process();
 
         // Execute as normally if includeChildren is false
         if (!$this->get('includeChildren') || null === $this->get('includeChildren')) {
-            return parent::process();
+            return;
         }
 
-        // Loop through all of a label's children and retrieve any child assets
-        $this->result;
+        $results = $this->getResult();
+        $labelId = $this->get('labelId');
+
+        $labelsChildren = $this->getClient()->getCommand('GetLabelsChildren', array(
+            'labelId' => $labelId
+        ))->execute();
+
+        if (count($labelsChildren['items']) > 0) {
+            // Loop through all of a label's children and retrieve any child assets
+            foreach($labelsChildren['items'] as $childKey => $childLabel) {
+                $childAssets = $this->getAssetsByLabelId($childLabel['id']);
+                $results[$childLabel['id']]['next_page'] = $childAssets['next_page'];
+                $results[$childLabel['id']]['items']     = $childAssets['items'];
+            }
+        }
+
+        $this->setResult($results);
     }
 
     /**
-     * Self referenced function to make it easy to retrieve children is needed
+     * Self referenced function to make it easy to retrieve children as needed
      */
     private function getAssetsByLabelId($labelId)
     {
-        return $this->getClient()->getCommand('GetLabelsAssets', arrya(
+        return $this->getClient()->getCommand('GetLabelsAssets', array(
             'labelId' => $labelId,
             'include' => $this->get('include'),
             'includeChildren' => $this->get('includeChildren')
