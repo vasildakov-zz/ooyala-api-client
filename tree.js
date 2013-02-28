7d199654bbefd0492e147142909d15cf4a71f9dc
Paul Redmond <paulrredmond@gmail.com>
2013-02-27 17:14:57 -0700

    Inital api build.

---
+++ tree.js
@@ -0,0 +1,11 @@
+var tree = [["SheKnows",,"",
+                [["OoyalaApi","SheKnows\/OoyalaApi.html","",
+                [["Command","SheKnows\/OoyalaApi\/Command.html","",
+                [["GetLabelsAssets","SheKnows\/OoyalaApi\/Command\/GetLabelsAssets.html"," < AbstractCommand",
+                [        ]],["UpdateAssetMetadata","SheKnows\/OoyalaApi\/Command\/UpdateAssetMetadata.html"," < AbstractCommand",
+                [        ]]        ]],["Plugin","SheKnows\/OoyalaApi\/Plugin.html","",
+                [["OoyalaSignature","SheKnows\/OoyalaApi\/Plugin\/OoyalaSignature.html","",
+                [        ]]        ]],["OoyalaClient","SheKnows\/OoyalaApi\/OoyalaClient.html"," < Client",
+                [        ]],["Signature","SheKnows\/OoyalaApi\/Signature.html","",
+                [        ]]        ]]        ]]        ]
+
